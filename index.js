b113d71e545d609390a7c31fdab1dc02e594adfd
Eli-Price <ewillp66@gmail.com>
2024-01-30 13:08:21 -0600

    changes

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const db = await open({
     driver: sqlite3.Database
 });
 
-
-await db.exec('CREATE TABLE IF NOT EXISTS messages ( id INTEGER PRIMARY KEY AUTOINCREMENT, client_offset TEXT UNIQUE, content TEXT);');
+await db.exec('CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, client_offset TEXT UNIQUE, content TEXT);');
 
 const app = express();
 const server = createServer(app);
@@ -29,15 +28,36 @@ app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     /*console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });*/
-    socket.on('chat message', (msg) => {
-        //console.log('message: ' + msg);
-        io.emit('chat message', msg)
+    socket.on('chat message', async (msg) => {
+        let result;
+        try {
+            result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+        } catch (e) {
+            // TODO handle error
+            return;
+        }
+        // include the offset with the message
+        io.emit('chat message', msg, result.lastID);
     });
+
+    if (!socket.recovered) {
+        // if the connection state recovery was not successful
+        try {
+            await db.each('SELECT id, content FROM messages WHERE id > ?',
+                [socket.handshake.auth.serverOffset || 0],
+                (_err, row) => {
+                    socket.emit('chat message', row.content, row.id);
+                }
+            )
+        } catch (e) {
+            // TODO handle error
+        }
+    }
 });
 
 server.listen(3000, () => {
